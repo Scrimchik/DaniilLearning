9883e32acd17072dd04b62a031ed526739c4415d
Yura <Scrimchikk@gmail.com>
2022-06-04 07:45:11 -0700

    Added trycatch

---
+++ TryCatch/Program.cs
@@ -0,0 +1,50 @@
+﻿using Liblary;
+using System;
+using System.Net.Http;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer writer = new Writer();
+
+            string text = Console.ReadLine();
+            string path = Console.ReadLine();
+
+            try
+            {
+                writer.WriteMessage(text, path);
+            }
+            catch (Exception)
+            {
+
+            }
+            catch ()
+
+            #region
+            //string url = Console.ReadLine();
+            //try
+            //{
+            //    using (HttpClient client = new HttpClient())
+            //    {
+            //        client.GetAsync(url);
+            //    }
+            //}
+            //catch (HttpRequestException ex)
+            //{
+            //    Console.WriteLine(ex.StatusCode);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"{ex.}");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Ты идиот");
+            //}
+            #endregion
+        }
+    }
+}
